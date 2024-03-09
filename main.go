1abf7ae51663652edd6b33ef04347e195767e54b
axelcarl <axelthescarecrow@gmail.com>
2024-03-09 11:06:54 +0100

    Organizing file structure

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -12,63 +11,27 @@ type apiConfig struct {
 	fileserverHits int
 }
 
-func middlewareCors(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) middlewareMetrics(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits++
-		next.ServeHTTP(w, r)
-	})
-}
-
 func main() {
 	const port string = "8080"
 
 	cfg := apiConfig{}
 
 	r := chi.NewRouter()
-
 	corsMux := middlewareCors(r)
+
+	fsHandler := cfg.middlewareMetrics(http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	r.Handle("/app", fsHandler)
+	r.Handle("/app/*", fsHandler)
+
+	r.Get("/healthz", handlerHealth)
+	r.Get("/metrics", cfg.handlerMetrics)
+	r.Get("/reset", cfg.handlerReset)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: corsMux,
 	}
 
-	r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(200)
-		w.Write([]byte("OK"))
-	})
-
-	r.Get("/metrics", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(200)
-
-		w.Write([]byte("Hits: " + strconv.Itoa(cfg.fileserverHits)))
-	})
-
-	r.Get("/reset", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(200)
-		cfg.fileserverHits = 0
-	})
-
-	r.Handle("/app", cfg.middlewareMetrics(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
-	r.Handle("/app/*", cfg.middlewareMetrics(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
-
 	log.Printf("Starting server on port: %s\n", port)
-
 	log.Fatal(srv.ListenAndServe())
 }
---
+++ reset.go
@@ -0,0 +1,9 @@
+package main
+
+import "net/http"
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	cfg.fileserverHits = 0
+}
